9a1c1189ce7ebfe3ac47b1b0d48dae7a3f729d45
luffy-studio <sarvagyasharma660@gmail.com>
2024-11-29 01:13:38 +0530

    Initial commit

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -31,10 +31,12 @@ const Footer = () => {
         <p>Contact</p>
       </div>
       <div className="gpt3__footer-links_div">
-        <h4>Get in touch</h4>
-        <p>Crechterwoord K12 182 DK Alknjkcb</p>
-        <p>085-132567</p>
-        <p>info@payme.net</p>
+        <ul>
+          <li> <a href="https://www.youtube.com/" className='cursoe-pointer text-white'>Get in touch</a></li>
+          <li>Crechterwoord K12 182 DK Alknjkcb</li>
+          <li>085-132567</li>
+          <li>info@payme.net</li>
+          </ul>
       </div>
     </div>
 
